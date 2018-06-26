da328b1363c8138364371f0184f4f542999b40a4
nilsonli <nilsonli@gmail.com>
2018-06-26 03:07:44 -0300

    Nilson Lima - Commit projeto TP 06 - implementando Classe Mecanico.java

---
+++ MotorWebApp/src/java/br/com/fatecpg/motorwebapp/web/Mecanico.java
@@ -0,0 +1,108 @@
+package br.com.fatecpg.motorwebapp.web;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Mecanico {
+    private long id;
+    private int cpf;
+    private String nome;
+
+public Mecanico(long id, int cpf, String nome) {
+         
+        this.id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+     public static ArrayList<Mecanico> getMecanico() throws SQLException, ClassNotFoundException
+    {   
+        String SQL = "SELECT * FROM MECANICO";
+        ArrayList<Mecanico> mecanic = new ArrayList<>();
+        ArrayList<Object[]> list = DatabaseConnector.getQuery(SQL, new Object[]{});
+        if(list.isEmpty())
+        {
+            return null;
+        }    
+        else
+        {
+            for(int i = 0; i < list.size(); i++)
+            {
+                Object row[] = list.get(i);
+                Mecanico m = new Mecanico(
+                    (long)row[0]
+                  , (int) row[1]
+                  , (String) row[2]);
+                    
+                mecanic.add(m);
+            }
+        }
+        return mecanic;
+    }
+    public static void addMecanico(int cpf, String nome) throws SQLException, ClassNotFoundException
+    {   
+        String SQL = "INSERT INTO MECANICO VALUES(default, ?, ?)";
+        Object parameters[] = {cpf, nome};
+        DatabaseConnector.setQuery(SQL, parameters);
+    }
+    public static void delMecanico(long id) throws SQLException, ClassNotFoundException
+    {   
+        String SQL = "DELETE FROM MECANICO WHERE ID=?";
+        Object parameters[] = {id};
+        DatabaseConnector.setQuery(SQL, parameters);
+    }
+    public static void altMecanico(int cpf, String nome, long id) throws SQLException, ClassNotFoundException
+    {   
+        String SQL = "UPDATE MECANICO SET cpf=?, nome=?, WHERE ID=?";
+        Object parameters[] = {cpf, nome, id};
+        DatabaseConnector.setQuery(SQL, parameters);
+    }
+    public static Mecanico getMecanicoAlterar(long id) throws SQLException, ClassNotFoundException
+    {   
+        String SQL = "SELECT * FROM MECANICO WHERE ID = ?";
+        Object parameters[] = {id};
+        ArrayList<Object[]> list = DatabaseConnector.getQuery(SQL, parameters);
+        if(list.isEmpty())
+        {
+            return null;
+        }    
+        else
+        {
+            Object row[] = list.get(0);
+            Mecanico m = new Mecanico(
+                    (long)row[0]
+                    , (int) row[1]
+                    , (String) row[2]);
+                    
+            return m;
+        }   
+    }
+  
+}
